b5faa6187fb5615c4d864629af0bea2a5d84932f
khasa <khasa@LAPTOP-RNHMEEHP>
2022-04-04 00:02:10 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FirstForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlayerName = new System.Windows.Forms.Label();
+            this.PlayerName_TextBox = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PlayerName
+            // 
+            this.PlayerName.AutoSize = true;
+            this.PlayerName.Location = new System.Drawing.Point(44, 51);
+            this.PlayerName.Name = "PlayerName";
+            this.PlayerName.Size = new System.Drawing.Size(84, 16);
+            this.PlayerName.TabIndex = 0;
+            this.PlayerName.Text = "Имя игрока:";
+            // 
+            // PlayerName_TextBox
+            // 
+            this.PlayerName_TextBox.Location = new System.Drawing.Point(134, 48);
+            this.PlayerName_TextBox.Name = "PlayerName_TextBox";
+            this.PlayerName_TextBox.Size = new System.Drawing.Size(226, 22);
+            this.PlayerName_TextBox.TabIndex = 1;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(285, 119);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Старт";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // FirstForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 177);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.PlayerName_TextBox);
+            this.Controls.Add(this.PlayerName);
+            this.MaximizeBox = false;
+            this.Name = "FirstForm";
+            this.Text = "Игра";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PlayerName;
+        private System.Windows.Forms.TextBox PlayerName_TextBox;
+        private System.Windows.Forms.Button Start;
+    }
+}
+
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class GameForm : Form
+    {
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        int i;
+        string c;
+        int tk;
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+            i = 300;
+            c = "5:00";
+            label1.Text = c;
+            timer1.Interval = 2000;
+            timer1.Tick += timer1_Tick;
+            timer1.Enabled = true;
+            timer1.Start();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tk = --i;
+            TimeSpan span = TimeSpan.FromMinutes(tk);
+            string label = span.ToString(@"hh\:mm");
+            label1.Text = label.ToString();
+            if (i < 0) timer1.Stop();
+        }
+
+        List<Image> images = new List<Image>();
+        List<PictureBox> pictureBoxes = new List<PictureBox>();
+        
+        public void images_add()
+        {
+            Random rnd = new Random();
+            DirectoryInfo dir = new DirectoryInfo($"{Environment.CurrentDirectory}\\Photos");
+            foreach (FileInfo file in dir.EnumerateFiles("*.jpg"))
+            {
+                images.Add(Image.FromFile(file.FullName));
+            }
+        }
+
+        public void AssignImagessToPicterBoxes()
+        {
+            images_add();
+            
+            foreach (PictureBox pictureBox in pictureBoxes)
+            {
+                //Label imageLabel = control as Label;
+                //if (imageLabel != null)
+                //{
+                //    int randomNumber = random.Next(images.Count);
+                //    imageLabel.Image = images[randomNumber];
+                //    images.RemoveAt(randomNumber);
+                //}
+            }
+        }
+
+
+
+    }
+}
